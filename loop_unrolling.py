926a8ab816b4f7db147fd8f811276516e92bfb22
Fernanda Laís Pereira de Souza <fernandalaisouza@hotmail.com>
2019-09-05 01:17:19 -0300

    DONE: matrizes, fork, envio de linha e coluna; TO DO: implementar func e popular results

--- loop_unrolling.py
+++ loop_unrolling.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 import os
+import numpy as np
 
 # deve ser chamada em paralelo
 def func(matriz):
@@ -27,35 +28,44 @@ def multiplica_matriz(mat1, mat2):
             # e em seguida adiciona a matResultado a soma das multiplicações
             matResultado[i].append(sum(listMult))
     return matResultado
+    
 
 #recebe e trata uma matriz
 def unroll(args, func, method, results):
-    mat1 = [[2, 3], [4, 6]]
+    #criando matrizes randomicamente usando as dimensões recebidas em args
+    m1 = np.random.randint(0,9,(args[0][0],args[0][1]))
+    m2 = np.random.randint(0,9,(args[1][0],args[1][1]))
 
     if method == 'proc':
         val = os.fork()
 
         if val == 0:
-            matriz_multiplicada = multiplica_matriz(args, mat1)
+            #matriz_multiplicada = multiplica_matriz(m1, m2)
             print("Matriz resultado")
-            for row in matriz_multiplicada:
-                #aqui se bota o fork ????
-                #results será a matriz resultado ???? se sim, como construir? não é com append
-                func(row)
+            for indice in len(m1[0]):
+                linha_m1 = m1[indice]
+                coluna_m2 = m2[:, indice]
+                
+                proc_linha = os.fork()
+                if proc_linha == 0:
+                    func(linha_m1, coluna_m2)
         else:
-            print("Matriz args")
-            for i in args:
+            """
+            print("Matriz 1")
+            for i in m1:
                 func(i)
             print()
 
-            print("Matriz auxiliar")
-            for i in mat1:
+            print("Matriz 2")
+            for i in m2:
                 func(i)
             print()            
-
+            """
 #armazena os retornos de func
 results = []
-#executa a chamada para somar
-unroll([[1,2],
-        [3,4]], func, 'proc', results)
+
+# envia qtd de linhas e colunas das matrizes
+
+unroll([[2,2],
+        [2,2]], func, 'proc', results)
 #unroll([[5,3],[1,6]], func, 'proc', results)]
